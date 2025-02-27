4ae282bfac41253244af4958f0ab5f97b07f0e7c
aqc1 <aqc1@pct.edu>
2025-02-27 16:47:13 -0500

    Base project started

---
+++ src/NoteLayout.tsx
@@ -0,0 +1,19 @@
+import { Navigate, Outlet, useOutletContext, useParams } from "react-router"
+import { Note } from "./App"
+
+type NotelayoutProps = {
+    notes: Note[]
+}
+
+export function NoteLayout({ notes }: NotelayoutProps) {
+    const { id } = useParams()
+    const note = notes.find(n => n.id === id)
+
+    if (note == null) return <Navigate to="/" replace />
+
+    return <Outlet context={note} />
+}
+
+export function useNote() {
+    return useOutletContext<Note>()
+}
